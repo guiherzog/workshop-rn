e66d4444d83157ce227263deb42064f3649d2e35
Guilherme Herzog <guilhermeherzog@gmail.com>
2017-09-20 20:48:43 -0300

    Adicionado comentários no código

--- NearMe/index.ios.js
+++ NearMe/index.ios.js
@@ -1,35 +1,34 @@
 /**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
+  Código-exemplo do Workshop de RN UFRJ
+**/
+// Imports dos elementos utilizados.
 import React, { Component } from 'react';
 import {
-  LayoutAnimation,
   AppRegistry,
   StyleSheet,
   Text,
   Image,
-  View,
   Alert,
   ScrollView,
   TouchableOpacity
 } from 'react-native';
 
 export default class NearMe extends Component {
+  // Inicializa as variaveis de estado.
   state = {
     photos: [],
     lat: '',
     lng: '',
   }
-
+  // Este método é chamado toda vez que o componente vai ser exibido na tela.
   componentWillMount(){
     this.getCurrentLocation();
   }
-
-  // Caso dê certo, retorna a position.
-  // Se der errado, exibe um alerta.
+  /* 
+    Pega a posição do usuário usando o GPS
+    Caso dê certo, retorna a position.
+    Se der errado, exibe um alerta.
+  */
   getCurrentLocation = () => {
     navigator.geolocation.getCurrentPosition(
       (position) => {
@@ -47,47 +46,55 @@ export default class NearMe extends Component {
     );
   }
 
+  // Método que acessa a API do instagram e pega as fotos recentes próximas de um local.
   getPhotosNearMe = async () => {
     const lat = this.state.lat;
     const lng = this.state.lng;
+
+    // Linha que acessa a API.
     const response = await fetch(`https://api.instagram.com/v1/media/search?lat=${lat}&lng=${lng}&access_token=52980424.ba4c844.3858d3cfc6dd40d4b3528e72e2deea6e&distance=5000`);
     const apiData= await response.json();
 
+    // Limpa o array recebido para usar apenas a URL.
     const photos = apiData.data.map((photo) => {
       return { url: photo.images.standard_resolution.url }
     })
 
+    // Altera o estado com as fotos recebidas.
     this.setState({ 
       photos
     })
   }
 
+  // Método responsável por converter o array de fotos em um array de componentes React.
   renderPhotos = () => {
     const photoComponents = this.state.photos.map((photo)=>{
       return (
         <Image key={photo.url} source={{uri: photo.url}} style={[styles.photo, {width: '90%', height: 300}]}/>
       )
     })
-    console.log(photoComponents);
     return photoComponents;
   }
 
+  // Este método é chamado toda vez que uma variável de estado é modificada.
   render() {
     return (
       <ScrollView contentContainerStyle={styles.contentContainer} style={styles.container}>
-        {this.renderPhotos()}
-        <Text style={styles.btnText}>{`${this.state.lat} ${this.state.lng}`}</Text>
-        <TouchableOpacity style={styles.button} onPress={this.getPhotosNearMe} >
-          <Text style={styles.btnText}>Procurar</Text>
+        <Text style={styles.text}>{`Lat=${this.state.lat} Lng=${this.state.lng}`}</Text>
+        <TouchableOpacity style={styles.button} onPress={this.getPhotosNearMe}>
+          <Text style={styles.text}>Procurar</Text>
         </TouchableOpacity>
+        {this.renderPhotos()}
       </ScrollView>
     );
   }
 }
 
+// Variável responsável por lidar com os estilos dos components acima.
 const styles = StyleSheet.create({
   contentContainer: {
     alignItems: 'center',
+    paddingTop: 20,
   },
   container: {
     flex: 1,
@@ -95,20 +102,22 @@ const styles = StyleSheet.create({
   },
   button: {
     height: 40,
+    width: 100,
+    margin: 20,
     borderRadius: 25,
     alignItems: 'center',
-    margin: 10,
     justifyContent: 'center',
-    width: 100,
     backgroundColor: '#0F7173',
   },
-  btnText: {
+  text: {
     color: 'white',
   },
   photo: {
     borderRadius: 10,
     marginVertical: 10,
+    backgroundColor: '#666',
   },
 });
 
+// Esta linha define qual é a tela inicial do aplicativo.
 AppRegistry.registerComponent('NearMe', () => NearMe);
